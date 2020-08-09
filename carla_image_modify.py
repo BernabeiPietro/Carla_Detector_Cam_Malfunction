e430b21780dd417c4aff6d1022a4c714186da11d
BernabeiPietro <pietrobernaei@gmail.com>
2020-08-09 16:47:11 +0200

    fix modify_photo.py e carla_image_modify.py

--- carla_image_modify.py
+++ carla_image_modify.py
@@ -1,48 +1,48 @@
-import os;
+import os
 import modify_photo as modp
 import manager_of_path
 
+
 def manage_image(mp):
     total_classes = 500
-    train_classes = 400;  # 166
+    train_classes = 400  # 166
     validation_classes = 100
     i = 0
     j = 0
-    # if os.path.exists(path):
     os.makedirs(mp.path_train_original, exist_ok=True)
     os.makedirs(mp.path_validation_modified, exist_ok=True)
     os.makedirs(mp.path_train_modified, exist_ok=True)
     os.makedirs(mp.path_validation_original, exist_ok=True)
     while i < train_classes:
-        name = str(i).zfill(4);
+        name = str(i).zfill(4)
         path_image = mp.get_image_path(name)
-        file = os.listdir(path_image);
-        list=modp.open_cv2(path_image,file)
-        j_modified= modp.blur(list, j, mp.path_train_modified)
-        j_original= modp.not_modified(list, j, mp.path_train_original)
+        file = os.listdir(path_image)
+        list = modp.open_cv2(path_image, file)
+        j_modified = modp.blur(list, j, mp.path_train_modified)
+        j_original = modp.not_modified(list, j, mp.path_train_original)
         if j_original != j_modified:
             print("error image processing")
-        j = j_original;
+        j = j_original
         print(path_image)
-        print(file);
-        i = i + 1;
+        print(file)
+        i = i + 1
     j = 0
 
     while i < validation_classes + train_classes:
-        name = str(i).zfill(4);
+        name = str(i).zfill(4)
         path_image = mp.get_image_path(name)
-        file = os.listdir(path_image);
+        file = os.listdir(path_image)
         list = modp.open_cv2(path_image, file)
         j_modified = modp.blur(list, j, mp.path_validation_modified)
         j_original = modp.not_modified(list, j, mp.path_validation_original)
         if j_original != j_modified:
             print("error image processing")
-        j = j_original;
+        j = j_original
         print(path_image)
-        print(file);
-        i = i + 1;
+        print(file)
+        i = i + 1
 
 
 path = "/home/bernabei/carla0.8.4/PythonClient/_out"
-mp=manager_of_path.ManagerOfPath(path)
+mp = manager_of_path.ManagerOfPath(path)
 manage_image(mp)
