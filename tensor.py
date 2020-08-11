5544dd89dca776ad12e3052631e262de9a0b0ef9
BernabeiPietro <pietrobernaei@gmail.com>
2020-08-11 13:10:37 +0200

    fix tensor e manager_of_path

--- tensor.py
+++ tensor.py
@@ -66,20 +66,21 @@ def classificator(mp,classes,path_checkpoint):
         callbacks=[cp_callback]
     )
     model.save(mp.get_path_classes(classes)["checkpoint"])
+    #print_result(epochs, history)
+
+
+def print_result(epochs, history):
     acc = history.history['accuracy']
     val_acc = history.history['val_accuracy']
     loss = history.history['loss']
     val_loss = history.history['val_loss']
-
     epochs_range = range(epochs)
-
     plt.figure(figsize=(8, 8))
     plt.subplot(1, 2, 1)
     plt.plot(epochs_range, acc, label='Training Accuracy')
     plt.plot(epochs_range, val_acc, label='Validation Accuracy')
     plt.legend(loc='lower right')
     plt.title('Training and Validation Accuracy')
-
     plt.subplot(1, 2, 2)
     plt.plot(epochs_range, loss, label='Training Loss')
     plt.plot(epochs_range, val_loss, label='Validation Loss')
