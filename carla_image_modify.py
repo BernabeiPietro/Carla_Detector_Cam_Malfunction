0b9a9b6eb97c7f442a8c7be41a692977193060b5
Bernabei <bernabei@lutezio.math.unifi.it>
2020-11-09 22:38:18 +0100

    tester

--- carla_image_modify.py
+++ carla_image_modify.py
@@ -5,7 +5,7 @@ import manager_of_path
 
 def manage_image(mp, classes_of_modified):
     total_classes = 24#500
-    train_classes = 1 # 400
+    train_classes = 24 # 400
     validation_classes =0 #100
     i = 0
     j = 0
@@ -136,10 +136,10 @@ def modify_photo(classes, mp, list_original, j, tv):
 
 
 
-path = "/media/pietro/Volume/Ubuntu/home/pietro/Documenti/Unifi/tirocinio/img/" #"/home/pietro/Documenti/Unifi/tirocinio/img"#"/home/bernabei/carla0.8.4/PythonClient/_out_prima_run/" #"/media/pietro/Volume/Ubuntu/home/pietro/Documenti/Unifi/tirocinio/img/"
+path = "/home/bernabei/carla0.8.4/PythonClient/_out/" #"/home/pietro/Documenti/Unifi/tirocinio/img"#"/home/bernabei/carla0.8.4/PythonClient/_out_prima_run/" #"/media/pietro/Volume/Ubuntu/home/pietro/Documenti/Unifi/tirocinio/img/"
 classes_of_modified = ["blur", "black", "brightness",  "200_death_pixels","nodemos","noise","sharpness","brokenlens","icelens","banding","50_death_pixels","greyscale","condensation","dirty_lens","chromaticaberration","rain"]
 mp = manager_of_path.ManagerOfPath(path, classes_of_modified, True)
-manage_image(mp, classes_of_modified[3:4])
+manage_image(mp, classes_of_modified)
 
 
 
